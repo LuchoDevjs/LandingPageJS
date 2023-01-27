eea8c19dc4702deef0eb1c98bf163dd8e386fa6f
Luciano Gonzalez <luchogonzalez080@gmail.com>
2023-01-27 15:40:39 -0300

    push 2023

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect } from "react";
 import { Text } from "@chakra-ui/react";
-import { getTitle } from "../helpers/getApi";
-//  to do conectar a un modelo
 
 const Title = () => {
-  useEffect(() => {
-    getTitle();
-  }, []);
   return (
     <Text
       fontSize={{ base: "2xl", md: "3xl", lg: "5xl" }}
@@ -14,7 +9,7 @@ const Title = () => {
       mt={12}
       letterSpacing={2}
     >
-      {getTitle().title}
+      ÚLTIMA FORMACIÓN PROFESIONAL 2022 MICROPIGMENTACIÓN
     </Text>
   );
 };
